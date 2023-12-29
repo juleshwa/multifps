145160a478bce9ccfd15db7adfaa71921dd74762
Juleshwa <juleshwa@gmail.com>
2023-12-29 22:30:42 +0700

    init

---
+++ Assets/Script/RoomManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+
+    public GameObject player;
+    [Space]
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected on Server");
+
+        PhotonNetwork.JoinLobby();
+
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("we are connected and in aroom -> Lobby");
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("we are connected and in a room ->joined");
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        if (PhotonNetwork.IsMasterClient) // Only the master client instantiates the player
+        {
+            Debug.Log("true masterClient");
+        } else
+        {
+            Debug.Log("other clent");
+        }
+    }
+}
